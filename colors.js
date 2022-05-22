d5cd767485c9b14fe5f417a16c475527287893a8
Franz Gatt <franz.gatt@student.uibk.ac.at>
2022-05-22 17:29:57 +0200

    humidity-Palette eingebaut & Beautify gemacht

--- colors.js
+++ colors.js
@@ -1,57 +1,70 @@
 const COLORS = {
     temperature: [{
-        min: -50,
-        max: -25,
-        color: "#784cff"
-    }, {
-        min: -25,
-        max: -20,
-        color: "#784cff"
-    }, {
-        min: -20,
-        max: -15,
-        color: "#0f5abe"
-    }, {
-        min: -15,
-        max: -10,
-        color: "#1380ff"
-    }, {
-        min: -10,
-        max: -5,
-        color: "#19cdff"
-    }, {
-        min: -5,
-        max: -0,
-        color: "#8fffff"
-    }, {
-        min: -0,
-        max: 5,
-        color: "#b0ffbc"
-    }, {
-        min: 5,
-        max: 10,
-        color: "#ffff73"
-    }, {
-        min: 10,
-        max: 15,
-        color: "#ffbe7d"
-    }, {
-        min: 15,
-        max: 20,
-        color: "#ff9b41"
-    }, {
-        min: 20,
-        max: 25,
-        color: "#ff5a41"
-    }, {
-        min: 25,
-        max: 30,
-        color: "#ff1e23"
-    }, {
-        min: 30,
-        max: 50,
-        color: "#fa3c96"
-    }],
+            min: -50,
+            max: -25,
+            color: "#784cff"
+        },
+        {
+            min: -25,
+            max: -20,
+            color: "#784cff"
+        },
+        {
+            min: -20,
+            max: -15,
+            color: "#0f5abe"
+        },
+        {
+            min: -15,
+            max: -10,
+            color: "#1380ff"
+        },
+        {
+            min: -10,
+            max: -5,
+            color: "#19cdff"
+        },
+        {
+            min: -5,
+            max: -0,
+            color: "#8fffff"
+        },
+        {
+            min: -0,
+            max: 5,
+            color: "#b0ffbc"
+        },
+        {
+            min: 5,
+            max: 10,
+            color: "#ffff73"
+        },
+        {
+            min: 10,
+            max: 15,
+            color: "#ffbe7d"
+        },
+        {
+            min: 15,
+            max: 20,
+            color: "#ff9b41"
+        },
+        {
+            min: 20,
+            max: 25,
+            color: "#ff5a41"
+        },
+        {
+            min: 25,
+            max: 30,
+            color: "#ff1e23"
+        },
+        {
+            min: 30,
+            max: 50,
+            color: "#fa3c96"
+        }
+    ],
     snowheight: [{
             min: 0,
             max: 1,
@@ -132,6 +145,47 @@ const COLORS = {
             min: 80,
             max: 200,
             color: "#ff3232"
+        }
+    ],
+    humidity: [{
+            min: 0,
+            max: 30,
+            color: "#EEE"
+        },
+        {
+            min: 30,
+            max: 40,
+            color: "#DDD"
+        },
+        {
+            min: 40,
+            max: 50,
+            color: "#C6C9CE"
+        },
+        {
+            min: 50,
+            max: 60,
+            color: "#BBB"
+        },
+        {
+            min: 60,
+            max: 70,
+            color: "#AAC"
+        },
+        {
+            min: 70,
+            max: 80,
+            color: "#9998DD"
+        },
+        {
+            min: 80,
+            max: 90,
+            color: "#8788EE"
+        },
+        {
+            min: 90,
+            max: 100,
+            color: "#7677E1"
         }
     ]
 }
